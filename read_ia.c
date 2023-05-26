7f3bbcef6a2c7a3b59cb62161789be3a9ef28567
Mojalefa Nkwana <mojalefa.emjay01@gmail.com>
2023-05-26 11:52:49 +0000

    re-ordered main

--- read_ia.c
+++ read_ia.c
@@ -12,7 +12,7 @@ char *read_ia(void)
 	char *buffer = NULL;
 	size_t buffer_size = 0;
 	ssize_t checked_chars = 0;
-	char *prompt = "#cisfun$ ";
+	char *prompt = "#cisfun$";
 
 	write(STDOUT_FILENO, prompt, _strlen(prompt));
 
